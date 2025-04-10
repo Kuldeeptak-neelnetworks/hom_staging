f4ae80b8fcf9b4ccabf7f207e5c1df4aaf797637
Kuldeeptak-neelnetworks <kuldeep@neelnetworks.com>
2025-04-10 12:55:13 +0530

    changes in animated Tool tip

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -68,16 +68,16 @@ export const AnimatedTooltip: React.FC<AnimatedTooltipProps> = ({ item }) => {
                 rotate: rotate,
                 whiteSpace: "nowrap",
               }}
-              className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+              className="absolute -top-16 first:-top-10 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
             >
               <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
               <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
-              <div>
-                <div>
+              <div className="flex align-baseline gap-3">
+                <div className="w-[50px] h-[50px]  overflow-hidden rounded-full">
                   <img
                     src={item?.picture}
                     alt={item?.name}
-                    className="rounded-full"
+                    className=" w-full h-full object-cover object-top"
                   />
                 </div>
                 <div>
